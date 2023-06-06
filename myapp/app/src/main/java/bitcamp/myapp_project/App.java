e09b6a815089332d6a7467d68c90a4d6b019c17d
wooki37 <lite1324@gmail.com>
2023-06-06 21:04:03 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp_project/App.java
+++ myapp/app/src/main/java/bitcamp/myapp_project/App.java
@@ -1,14 +1,13 @@
-package bitcamp.myapp;
+package bitcamp.myapp_project;
 
-// 코드 본문에서 사용할 클래스가 어떤 패키지의 클래스인지 지정한다.
 import bitcamp.myapp.handler.MemberHandler;
 import bitcamp.util.Prompt;
 
 public class App {
 
   public static void main(String[] args) {
+
     printTitle();
-    // 키보드 스캐너 준비
 
     while (MemberHandler.available()) {
       MemberHandler.inputMember();
@@ -23,16 +22,15 @@ public class App {
   }
 
   static void printTitle() {
-    System.out.println("나의 목록 관리 시스템"); // <- 문자열 스트림 리터럴
-    System.out.println("----------------------------");
+    System.out.println("동물병원 보호자 목록");
+    System.out.println("----------------------------------");
   }
 
   static boolean promptContinue() {
-    String response = Prompt.inputString("계속 하시겠습니까?(Y/n) ");
+    String response = Prompt.inputString("입력이 끝나셨습니까?(Y/n) ");
     if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
       return false;
     }
     return true;
   }
-
 }

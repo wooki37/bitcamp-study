79c1c8193598c0556734d31c098b970d5ab6a5dd
wooki37 <lite1324@naver.com>
2023-09-01 17:40:50 +0900

    .

---
+++ myapp/app-68-server/src/main/java/bitcamp/myapp/dao/MySQLMemberDao.java
@@ -0,0 +1,56 @@
+package bitcamp.myapp.dao;
+
+import bitcamp.myapp.vo.Member;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MySQLMemberDao implements MemberDao {
+
+  SqlSessionFactory sqlSessionFactory;
+
+  public MySQLMemberDao(SqlSessionFactory sqlSessionFactory) {
+    System.out.println("MYSQLMemberDao() 호출되었다~~");
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  @Override
+  public void insert(Member member) {
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);
+    sqlSession.insert("bitcamp.myapp.dao.MemberDao.insert", member);
+  }
+
+  @Override
+  public List<Member> findAll() {
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);
+    return sqlSession.selectList("bitcamp.myapp.dao.MemberDao.findAll");
+  }
+
+  @Override
+  public Member findBy(int no) {
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);
+    return sqlSession.selectOne("bitcamp.myapp.dao.MemberDao.findBy", no);
+  }
+
+  @Override
+  public Member findByEmailAndPassword(Member member) {
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);
+    return sqlSession.selectOne("bitcamp.myapp.dao.MemberDao.findByEmailAndPassword", member);
+  }
+
+  @Override
+  public int update(Member member) {
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);
+    return sqlSession.update("bitcamp.myapp.dao.MemberDao.update", member);
+  }
+
+  @Override
+  public int delete(int no) {
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);
+    return sqlSession.delete("bitcamp.myapp.dao.MemberDao.delete", no);
+  }
+
+}
